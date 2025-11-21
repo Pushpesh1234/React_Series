8c26eeda9e300ea47f7cc1930a41038058d5f8cf
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-11-21 08:00:48 +0000

    fetch with useState

--- src/components/FetchPostList.jsx
+++ src/components/FetchPostList.jsx
@@ -1,10 +1,10 @@
 import { Card } from "./PostCreated"
 
-const FetchPostList=({onGetInitalPost})=>{
+const FetchPostList=()=>{
     return(
         <center className="message-fetch-post">
             <h2>No Post Available</h2>
-            <button className="bg-primary bt-text" onClick={onGetInitalPost} >Fetch Post From Server</button>
+            <button className="bg-primary bt-text" >Fetch Post From Server</button>
         </center>
        
     )
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,19 +2,25 @@ import { Card } from "./PostCreated"
 import { useContext } from "react"
 import { LPost } from '../store/PostListStore'
 import FetchPostList from "./FetchPostList"
+import {useState} from "react"
 
 export const PostList = () => {
 
+
     const { currentPost,onInitialPost } = useContext(LPost)
-    const onfetchHandler=(e)=>{
-         e.preventDefault()
-       fetch("https://dummyjson.com/posts")
+    const [isFetched,setIsFetched]=useState(false)
+
+     if(!isFetched){
+        fetch("https://dummyjson.com/posts")
        .then((res)=>res.json())
        .then((data)=>{onInitialPost(data.posts)})
-    }
+       setIsFetched(true)
+     }
+       
+    
     return (
         <>
-           { currentPost.length === 0 && <FetchPostList onGetInitalPost={onfetchHandler} />}
+           { currentPost.length === 0 && <FetchPostList />}
 
             {currentPost.map((post, i) => {
                 return <Card key={i} post={post} id={i}></Card>
