3412d216c3b1d5861e96741d2a4ba3e129bbbbb4
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-11-22 08:10:30 +0000

    LoadingState & CleanUp

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,28 +1,36 @@
 import { Card } from "./PostCreated"
-import { useContext } from "react"
+import { useContext,useEffect} from "react"
 import { LPost } from '../store/PostListStore'
 import FetchPostList from "./FetchPostList"
 import {useState} from "react"
+import LoadingState from "./LoadingState"
 
 export const PostList = () => {
 
 
     const { currentPost,onInitialPost } = useContext(LPost)
-    const [isFetched,setIsFetched]=useState(false)
-
-     if(!isFetched){
+    const[Loading,setLoading]=useState(true)
+    
+      useEffect(()=>{
+        // setLoading(true)
         fetch("https://dummyjson.com/posts")
        .then((res)=>res.json())
-       .then((data)=>{onInitialPost(data.posts)})
-       setIsFetched(true)
-     }
-       
+       .then((data)=>{onInitialPost(data.posts)
+        setLoading(false)
+       });
+       return ()=>{
+         console.log("useEffect is cleaned up")
+       };
+      },[]) // ← Empty array = run ONLY once on mount
+     
+
     
     return (
         <>
-           { currentPost.length === 0 && <FetchPostList />}
+         {Loading&&<LoadingState/>}
+           {!Loading&& currentPost.length === 0 && <FetchPostList />}
 
-            {currentPost.map((post, i) => {
+            {!Loading&&currentPost.map((post, i) => {
                 return <Card key={i} post={post} id={i}></Card>
             })}
 
